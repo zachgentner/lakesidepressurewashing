947b46b15afa33e177c459b7e1b61b97af70f95a
Zach Gentner <zach.gentner@gmail.com>
2023-08-29 21:06:46 -0400

    updated to webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
 
 gulp.task("default", async function(){
     return console.log("running.");
 });
 
 gulp.task('imageMin', () =>
-	gulp.src('src/img/*')
+	gulp.src('img/*.*')
 		.pipe(imagemin())
-		.pipe(gulp.dest('img'))
+		.pipe(gulp.dest('minified'))
 );
 
 gulp.task ("copyHTML", async function(){
 	gulp.src('src/*html')
 	.pipe(gulp.dest('dist'));
 });
+ 
+gulp.task('toWebp', () =>
+    gulp.src('img/*')
+        .pipe(webp())
+        .pipe(gulp.dest('dist'))
+);
